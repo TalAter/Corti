b8d67fc5baa6c71a8897992d4ec35460645a0b0d
Tal Ater <tal@talater.com>
2024-06-03 16:30:40 -0600

    WIP: Rewrite corti as a class

--- src/corti.js
+++ src/corti.js
@@ -4,226 +4,93 @@
 //! license : MIT
 //! https://github.com/TalAter/Corti
 
-(function (undefined) {
-  // Save a reference to the global object (window in the browser)
-  const _root = this; // @TODO: Consider globalThis
+import DOMException from './DOMException';
 
-  // Holds the browser's implementation
-  let _originalSpeechRecognition = false;
+class corti {
+  #maxAlternatives = 1;
 
-  // Patch DOMException
-  var DOMException = DOMException || TypeError;
+  #lang = '';
 
-  // Speech Recognition attributes
-  let _maxAlternatives = 1;
-  let _lang = "";
-  let _continuous = false;
-  let _interimResults = false;
+  #continuous = false;
 
-  const newSpeechRecognition = function () {
-    const _self = this;
-    const _listeners = document.createElement("div");
-    _self._started = false;
-    _self._soundStarted = false;
-    _self.eventListenerTypes = ["start", "soundstart", "end", "result"];
-    _self.maxAlternatives = 1;
+  #interimResults = false;
 
-    // Add listeners for events registered through attributes (e.g. recognition.onend = function) and not as proper listeners
-    _self.eventListenerTypes.forEach(function (eventName) {
-      _listeners.addEventListener(
-        eventName,
-        function () {
-          if (typeof _self[`on${eventName}`] === "function") {
-            _self[`on${eventName}`].apply(_listeners, arguments);
-          }
-        },
-        false,
-      );
-    });
+  #started = false;
 
-    Object.defineProperty(this, "maxAlternatives", {
-      get() {
-        return _maxAlternatives;
-      },
-      set(val) {
-        if (typeof val === "number") {
-          _maxAlternatives = Math.floor(val);
-        } else {
-          _maxAlternatives = 0;
-        }
-      },
-    });
+  #soundStarted = false;
 
-    Object.defineProperty(this, "lang", {
-      get() {
-        return _lang;
-      },
-      set(val) {
-        if (val === undefined) {
-          val = "undefined";
-        }
-        _lang = val.toString();
-      },
-    });
+  constructor() {
+    this.#started = false;
+  }
 
-    Object.defineProperty(this, "continuous", {
-      get() {
-        return _continuous;
-      },
-      set(val) {
-        _continuous = Boolean(val);
-      },
-    });
+  get maxAlternatives() {
+    return this.#maxAlternatives;
+  }
 
-    Object.defineProperty(this, "interimResults", {
-      get() {
-        return _interimResults;
-      },
-      set(val) {
-        _interimResults = Boolean(val);
-      },
-    });
+  set maxAlternatives(val) {
+    if (typeof val === 'number') {
+      this.#maxAlternatives = Math.floor(val);
+    } else {
+      this.#maxAlternatives = 0;
+    }
+  }
 
-    this.start = function () {
-      if (_self._started) {
-        throw new DOMException(
-          "Failed to execute 'start' on 'SpeechRecognition': recognition has already started.",
-        );
-      }
-      _self._started = true;
-      // Create and dispatch an event
-      const event = document.createEvent("CustomEvent");
-      event.initCustomEvent("start", false, false, null);
-      _listeners.dispatchEvent(event);
-    };
+  get lang() {
+    return this.#lang;
+  }
 
-    this.abort = function () {
-      if (!_self._started) {
-        return;
-      }
-      _self._started = false;
-      _self._soundStarted = false;
-      // Create and dispatch an event
-      const event = document.createEvent("CustomEvent");
-      event.initCustomEvent("end", false, false, null);
-      _listeners.dispatchEvent(event);
-    };
+  set lang(val) {
+    if (val === undefined) {
+      this.#lang = 'undefined';
+    } else {
+      this.#lang = val.toString();
+    }
+  }
 
-    this.stop = function () {
-      return _self.abort();
-    };
+  get continuous() {
+    return this.#continuous;
+  }
 
-    this.isStarted = function () {
-      return _self._started;
-    };
+  set continuous(val) {
+    this.#continuous = Boolean(val);
+  }
 
-    this.say = function (sentence) {
-      if (!_self._started) {
-        return;
-      }
-      // Create some speech alternatives
-      const results = [];
-      let commandIterator;
-      let etcIterator;
-      const itemFunction = function (index) {
-        if (undefined === index) {
-          throw new DOMException(
-            "Failed to execute 'item' on 'SpeechRecognitionResult': 1 argument required, but only 0 present.",
-          );
-        }
-        index = Number(index);
-        if (isNaN(index)) {
-          index = 0;
-        }
-        if (index >= this.length) {
-          return null;
-        }
-        return this[index];
-      };
-      for (
-        commandIterator = 0;
-        commandIterator < _maxAlternatives;
-        commandIterator++
-      ) {
-        let etc = "";
-        for (etcIterator = 0; etcIterator < commandIterator; etcIterator++) {
-          etc += " and so on";
-        }
-        results.push(sentence + etc);
-      }
+  get interimResults() {
+    return this.#interimResults;
+  }
 
-      // Create the start event
-      const startEvent = document.createEvent("CustomEvent");
-      startEvent.initCustomEvent("result", false, false, {
-        sentence,
-      }); // @TODO: Is this using deprecated functionality?
-      startEvent.resultIndex = 0;
-      startEvent.results = {
-        item: itemFunction,
-        0: {
-          item: itemFunction,
-          final: true, // @TODO: Should this be isFinal?
-        },
-      };
-      for (
-        commandIterator = 0;
-        commandIterator < _maxAlternatives;
-        commandIterator++
-      ) {
-        startEvent.results[0][commandIterator] = {
-          transcript: results[commandIterator],
-          confidence: Math.max(1 - 0.01 * commandIterator, 0.001),
-        };
-      }
-      Object.defineProperty(startEvent.results, "length", {
-        get() {
-          return 1;
-        },
-      });
-      Object.defineProperty(startEvent.results[0], "length", {
-        get() {
-          return _maxAlternatives;
-        },
-      });
-      startEvent.interpretation = null;
-      startEvent.emma = null;
-      _listeners.dispatchEvent(startEvent);
+  set interimResults(val) {
+    this.#interimResults = Boolean(val);
+  }
 
-      // Create soundstart event
-      if (!_self._soundStarted) {
-        _self._soundStarted = true;
-        const soundStartEvent = document.createEvent("CustomEvent");
-        soundStartEvent.initCustomEvent("soundstart", false, false, null);
-        _listeners.dispatchEvent(soundStartEvent);
-      }
+  isStarted() {
+    return this.#started;
+  }
 
-      // stop if not set to continuous mode
-      if (!_self.continuous) {
-        _self.abort();
-      }
-    };
+  start() {
+    if (this.#started) {
+      throw new DOMException("Failed to execute 'start' on 'SpeechRecognition': recognition has already started.");
+    }
 
-    this.addEventListener = function (event, callback) {
-      _listeners.addEventListener(event, callback, false);
-    };
-  };
+    this.#started = true;
 
-  // Expose functionality
-  _root.Corti = {
-    patch() {
-      if (_originalSpeechRecognition === false) {
-        _originalSpeechRecognition =
-          _root.SpeechRecognition ||
-          _root.webkitSpeechRecognition ||
-          _root.mozSpeechRecognition ||
-          _root.msSpeechRecognition ||
-          _root.oSpeechRecognition;
-      }
-      _root.SpeechRecognition = newSpeechRecognition;
-    },
+    // @TODO: Emit a start event
+  }
 
-    unpatch() {
-      _root.SpeechRecognition = _originalSpeechRecognition;
-    },
-  };
-}).call(this);
+  abort() {
+    if (!this.#started) {
+      return;
+    }
+    this.#started = false;
+    this.#soundStarted = false;
+
+    // @TODO: Emit an end event
+  }
+
+  stop() {
+    // @TODO: Implement stop's behavior according to the spec. Unlike abort, stop will attempt to return a SpeechRecognitionResult using the audio captured so far.
+    return this.abort();
+  }
+}
+
+export default corti;
