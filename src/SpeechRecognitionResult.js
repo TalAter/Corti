93ef09412c8d11510a55cea20dc489c54addba0b
Tal Ater <tal@talater.com>
2024-06-05 16:09:27 -0600

    Implement `SpeechRecognitionResult`

--- src/SpeechRecognitionResult.js
+++ src/SpeechRecognitionResult.js
@@ -1,2 +1,32 @@
-class SpeechRecognitionResult {}
+class SpeechRecognitionResult {
+  constructor(alternatives = []) {
+    alternatives.forEach((alternative, index) => {
+      this[index] = alternative;
+    });
+    this.length = alternatives.length;
+    this.isFinal = true;
+  }
+
+  get isFinal() {
+    return this.isFinal;
+  }
+
+  item(index) {
+    if (arguments.length === 0) {
+      throw new TypeError(
+        "Failed to execute 'item' on 'SpeechRecognitionResult': 1 argument required, but only 0 present."
+      );
+    }
+    if (typeof index !== 'number' || Number.isNaN(index)) {
+      return this[0] || null;
+    }
+    return this[index] || null;
+  }
+
+  *[Symbol.iterator]() {
+    for (let i = 0; i < this.length; i += 1) {
+      yield this[i];
+    }
+  }
+}
 export default SpeechRecognitionResult;
