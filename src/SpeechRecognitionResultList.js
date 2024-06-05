8539b7ef9d76c074310c915621be55246492acb9
Tal Ater <tal@talater.com>
2024-06-05 15:24:09 -0600

    `SpeechRecognitionResultList.item()` behavior implemented when argument is not a number or missing
    - throws an error when called with no arguments
    - returns the first result if called with an argument that isn't a number

--- src/SpeechRecognitionResultList.js
+++ src/SpeechRecognitionResultList.js
@@ -7,6 +7,14 @@ class SpeechRecognitionResultList {
   }
 
   item(index) {
+    if (arguments.length === 0) {
+      throw new TypeError(
+        "Failed to execute 'item' on 'SpeechRecognitionResultList': 1 argument required, but only 0 present."
+      );
+    }
+    if (typeof index !== 'number' || Number.isNaN(index)) {
+      return this[0] || null;
+    }
     return this[index] || null;
   }
 
