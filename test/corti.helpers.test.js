516f88a255189acff8e012629d651ec0092f0f08
Tal Ater <tal@talater.com>
2024-06-06 01:11:09 -0600

    Add extensive testing to corti.say() and its behavior around alternatives

--- test/corti.helpers.test.js
+++ test/corti.helpers.test.js
@@ -36,4 +36,56 @@ describe('SpeechRecognition.say()', () => {
     recognition.say(getSentence(1));
     expect(spyFn1).toBeCalledTimes(2);
   });
+
+  it('should have the text of the first argument as the transcript of the first SpeechRecognitionAlternative', () => {
+    recognition.say(getSentence(0));
+    expect(getLastSpiedSpeechRecognitionEvent(spyFn1).results.item(0).item(0).transcript).toEqual(
+      "You can't take the sky from me"
+    );
+  });
+
+  describe('should return a SpeechRecognitionResult with a length equal to the maxAlternatives setting', () => {
+    it('when called with no alternatives', () => {
+      recognition.say(getSentence(0));
+      expect(getLastSpiedSpeechRecognitionEvent(spyFn1).results.item(0).length).toEqual(1);
+      recognition.maxAlternatives = 4;
+      recognition.say(getSentence(0));
+      expect(getLastSpiedSpeechRecognitionEvent(spyFn1).results.item(0).length).toEqual(4);
+    });
+
+    it('when called with exact number of alternatives', () => {
+      recognition.say(getSentence(0));
+      expect(getLastSpiedSpeechRecognitionEvent(spyFn1).results.item(0).length).toEqual(1);
+      recognition.maxAlternatives = 3;
+      recognition.say([getSentence(0), getSentence(1), getSentence(2)]);
+      expect(getLastSpiedSpeechRecognitionEvent(spyFn1).results.item(0).length).toEqual(3);
+    });
+
+    it('when called with too many alternatives', () => {
+      recognition.maxAlternatives = 3;
+      recognition.say([getSentence(0), getSentence(1), getSentence(2), getSentence(3)]);
+      expect(getLastSpiedSpeechRecognitionEvent(spyFn1).results.item(0).length).toEqual(3);
+    });
+
+    it('when called with too few alternatives', () => {
+      recognition.maxAlternatives = 3;
+      recognition.say([getSentence(0), getSentence(1)]);
+      expect(getLastSpiedSpeechRecognitionEvent(spyFn1).results.item(0).length).toEqual(3);
+    });
+  });
+
+  it('should use the alternatives provided as needed by maxAlternatives', () => {
+    recognition.maxAlternatives = 3;
+    recognition.say([getSentence(0), getSentence(1)]);
+    expect(getLastSpiedSpeechRecognitionEvent(spyFn1).results.item(0).item(0).transcript).toEqual(getSentence(0));
+    expect(getLastSpiedSpeechRecognitionEvent(spyFn1).results.item(0).item(1).transcript).toEqual(getSentence(1));
+  });
+
+  it('should create additional SpeechRecognitionAlternative objects when needed', () => {
+    recognition.maxAlternatives = 4;
+    recognition.say([getSentence(0), getSentence(1)]);
+    expect(getLastSpiedSpeechRecognitionEvent(spyFn1).results.item(0).item(3).transcript).toEqual(
+      `${getSentence(0)} and so on and so forth`
+    );
+  });
 });
